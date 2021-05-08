40b813b1b804d35fb1ae18430af7b68b77b1b9d9
prLorence <paulorencereyes@gmail.com>
2021-05-08 10:19:48 +0800

    completed the javascript exercsises in fundamentals

--- javascript-exercises/leapYears/leapYears.js
+++ javascript-exercises/leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+// if years are divisible by 4, return true
+// if years are divisible by 4 AND 400, return true
+// if years are divisible by 100 but not 4, return false
+// else return false
 
-}
+
+const leapYears = function(yearVal) {
+
+  if (yearVal % 4 == 0 && yearVal % 100 !== 0 || yearVal % 400 === 0) {
+    return true;
+  } else {
+    return false
+  }
+ 
+
+
+} 
 
 module.exports = leapYears
--- javascript-exercises/tempConversion/tempConversion.js
+++ javascript-exercises/tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const ftoc = function() {
+// for conversion, we need two parameters, one for celsius and one for fahrenheit
+// we need to round off to nearest tenths
 
-}
 
-const ctof = function() {
+const ftoc = function(fahr) {
 
+  let unRounded = (fahr - 32) * (5 / 9);
+  let rounded = Math.round(unRounded * 10) / 10;
+
+  return rounded
+} 
+
+const ctof = function(cel) {
+  let unRoundedC = cel * (9 / 5) + 32;
+  let roundedC = Math.round(unRoundedC * 10) / 10;
+
+  return roundedC
 }
 
 module.exports = {
