9b6c0aafec024e11e09527195561941481589263
prLorence <paulorencereyes@gmail.com>
2021-05-19 11:20:15 +0800

    added the solutions for today's TOP js exercises

--- javascript-exercises/palindromes/palindromes.js
+++ javascript-exercises/palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function() {
+// check if
 
+const clean = (str) => str.toLowerCase().replace(/[\W_]/g, "")
+
+const palindromes = function (str) {
+  const wordSet = clean(str)
+
+  return wordSet.split("").reduce((match, acc, curr) => {
+    if (!match) {
+      return false
+    }
+
+    return acc === wordSet[wordSet.length - 1 - curr]
+  }, true)
 }
-
 module.exports = palindromes
