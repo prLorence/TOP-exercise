69c293b64fc32b6b24a3b721308250465753b6aa
prLorence <paulorencereyes@gmail.com>
2021-06-05 11:35:00 +0800

    written the main page, might tweak some later

---
+++ restaurant-page/src/home.js
@@ -0,0 +1,31 @@
+function homePage() {
+  const homeContainer = document.createElement("div")
+  homeContainer.classList.add("content")
+
+  const landingPhoto = document.createElement("img")
+  landingPhoto.src = "./images/burger.jpg"
+  landingPhoto.classList.add("landing-photo")
+
+  homeContainer.appendChild(tagLine("Food at"))
+  homeContainer.appendChild(tagLine("Your Fingertips"))
+  homeContainer.appendChild(landingPhoto)
+
+  return homeContainer
+}
+
+function tagLine(message) {
+  const heroTitle = document.createElement("h2")
+  heroTitle.classList.add("tagline")
+  heroTitle.setAttribute("id", "tagline")
+
+  heroTitle.textContent = message
+  return heroTitle
+}
+
+function home() {
+  const main = document.getElementById("main")
+  main.textContent = ""
+  main.appendChild(homePage())
+}
+
+export default home
