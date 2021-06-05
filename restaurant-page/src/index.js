1c56bd585b1a3d1f79285f3acee30a374c360c69
prLorence <paulorencereyes@gmail.com>
2021-06-05 11:36:09 +0800

    imported the loadWebsite

--- restaurant-page/src/index.js
+++ restaurant-page/src/index.js
@@ -1,1 +1,10 @@
-console.log("hello")
+// create the mockup so I can style the page, the homepage should be fine
+// remove all of the elements and append it using js
+// this is mobile-first approach
+// 1. create the navbar and icon (text), hamburger icon
+// 2. style the background
+// 3. create the content
+
+import loadWebsite from "./mainWebsite"
+
+loadWebsite()
