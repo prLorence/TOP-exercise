f068f0e9232b89d037d18ec4b1f605916a29e8b7
prLorence <paulorencereyes@gmail.com>
2021-06-16 11:06:50 +0800

    just checked how would checkbox texts would look like in html

--- todo-list/dist/main.js
+++ todo-list/dist/main.js
@@ -1,1 +1,136 @@
-(()=>{"use strict";let t=[];const e=function(){let e;return event.preventDefault(),e=new class{constructor(t,e){this.title=form.title.value,this.description=form.description.value}}(undefined,undefined),t.push(e),void function(){const e=document.querySelector(".task-container");document.querySelectorAll("#task").forEach((t=>e.removeChild(t)));for(let e=0;e<t.length;e++)createTask(t[e])}()},d=function(){const t=document.querySelector(".dashboard"),d=document.createElement("div");d.setAttribute("id","modal"),d.classList.add("modal");const n=document.createElement("div");n.classList.add("field-div"),n.setAttribute("id","field-div");const i=document.getElementById("project-btn"),s=document.createElement("section");s.classList.add("task-header"),s.setAttribute("id","task-header");const a=document.createElement("form");a.setAttribute("action","./formDetails.js"),a.setAttribute("id","form"),a.classList.add("modal-content");const c=document.createElement("span");c.classList.add("close-button"),c.textContent="X";const o=document.createElement("p");o.textContent="Add Project";const l=document.createElement("p"),r=document.createElement("input");r.classList.add("task-title"),r.setAttribute("type","text"),r.setAttribute("name","title"),r.setAttribute("placeholder","Task Title"),r.setAttribute("id","task");const u=document.createElement("p"),p=document.createElement("input");p.classList.add("task-description"),p.setAttribute("type","text"),p.setAttribute("name","description"),p.setAttribute("placeholder","Task Description"),p.setAttribute("id","task");const m=document.createElement("button");return m.classList.add("add-task"),m.setAttribute("id","add-task"),m.textContent="Add Task",m.addEventListener("click",e),i.addEventListener("click",(()=>{d.style.display="block"})),m.addEventListener("click",(()=>{d.style.display="none",t.removeChild(d)})),c.addEventListener("click",(()=>{d.style.display="none",t.removeChild(d)})),window.addEventListener("click",(()=>{event.target==d&&(d.style.display="none",t.removeChild(d))})),l.appendChild(r),u.appendChild(p),n.appendChild(l),n.appendChild(u),s.appendChild(c),s.appendChild(o),a.appendChild(s),a.appendChild(n),a.appendChild(m),d.appendChild(a),t.appendChild(i),t.appendChild(d),t};!function(){const t=document.querySelector(".container"),e=document.createElement("div");e.classList.add("dashboard");const n=document.createElement("button");n.classList.add("project-button","modal-btn"),n.setAttribute("id","project-btn"),n.textContent="Add Project",t.appendChild(e),e.appendChild(n),n.addEventListener("click",d)}()})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/UIcomp.js":
+/*!***********************!*\
+  !*** ./src/UIcomp.js ***!
+  \***********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _createProjectBtn__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./createProjectBtn */ \"./src/createProjectBtn.js\");\n\r\n\r\nfunction createDashBoard() {\r\n\tconst container = document.querySelector(\".container\");\r\n\r\n\tconst dashBoard = document.createElement(\"div\");\r\n\tdashBoard.classList.add(\"dashboard\");\r\n\r\n\tconst addProjectBtn = document.createElement(\"button\");\r\n\taddProjectBtn.classList.add(\"project-button\", \"modal-btn\");\r\n\taddProjectBtn.setAttribute(\"id\", \"project-btn\");\r\n\taddProjectBtn.textContent = \"Add Project\";\r\n\r\n\tcontainer.appendChild(dashBoard);\r\n\tdashBoard.appendChild(addProjectBtn);\r\n\r\n\taddProjectBtn.addEventListener(\"click\", _createProjectBtn__WEBPACK_IMPORTED_MODULE_0__.default);\r\n\r\n\treturn container;\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (createDashBoard);\r\n\n\n//# sourceURL=webpack://todo-list/./src/UIcomp.js?");
+
+/***/ }),
+
+/***/ "./src/createProjectBtn.js":
+/*!*********************************!*\
+  !*** ./src/createProjectBtn.js ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _formDetails__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./formDetails */ \"./src/formDetails.js\");\n\r\n\r\nfunction createProject() {\r\n\tconst dashboard = document.querySelector(\".dashboard\");\r\n\r\n\tconst formModal = document.createElement(\"div\");\r\n\tformModal.setAttribute(\"id\", \"modal\");\r\n\tformModal.classList.add(\"modal\");\r\n\r\n\tconst fieldDiv = document.createElement(\"div\");\r\n\tfieldDiv.classList.add(\"field-div\");\r\n\tfieldDiv.setAttribute(\"id\", \"field-div\");\r\n\r\n\tconst addProjectBtn = document.getElementById(\"project-btn\");\r\n\r\n\tconst taskHeaderSection = document.createElement(\"section\");\r\n\ttaskHeaderSection.classList.add(\"task-header\");\r\n\ttaskHeaderSection.setAttribute(\"id\", \"task-header\");\r\n\r\n\tconst formContent = document.createElement(\"form\");\r\n\tformContent.setAttribute(\"action\", \"./formDetails.js\");\r\n\tformContent.setAttribute(\"id\", \"form\");\r\n\tformContent.classList.add(\"modal-content\");\r\n\r\n\tconst closeBtn = document.createElement(\"span\");\r\n\tcloseBtn.classList.add(\"close-button\");\r\n\tcloseBtn.textContent = \"X\";\r\n\r\n\tconst taskTitle = document.createElement(\"p\");\r\n\ttaskTitle.textContent = \"Add Project\";\r\n\r\n\t// Creates task title input field\r\n\tconst taskTitlePara = document.createElement(\"p\");\r\n\tconst taskTitleField = document.createElement(\"input\");\r\n\ttaskTitleField.classList.add(\"task-title\");\r\n\ttaskTitleField.setAttribute(\"type\", \"text\");\r\n\ttaskTitleField.setAttribute(\"name\", \"title\");\r\n\ttaskTitleField.setAttribute(\"placeholder\", \"Task Title\");\r\n\ttaskTitleField.setAttribute(\"id\", \"task\");\r\n\r\n\t// Creates task description input field\r\n\tconst taskDescPara = document.createElement(\"p\");\r\n\tconst taskDescField = document.createElement(\"input\");\r\n\ttaskDescField.classList.add(\"task-description\");\r\n\ttaskDescField.setAttribute(\"type\", \"text\");\r\n\ttaskDescField.setAttribute(\"name\", \"description\");\r\n\ttaskDescField.setAttribute(\"placeholder\", \"Task Description\");\r\n\ttaskDescField.setAttribute(\"id\", \"task\");\r\n\r\n\tconst addTaskBtn = document.createElement(\"button\");\r\n\taddTaskBtn.classList.add(\"add-task\");\r\n\taddTaskBtn.setAttribute(\"id\", \"add-task\");\r\n\taddTaskBtn.textContent = \"Add Task\";\r\n\r\n\t// Click event listeners\r\n\r\n\taddTaskBtn.addEventListener(\"click\", _formDetails__WEBPACK_IMPORTED_MODULE_0__.default);\r\n\r\n\taddProjectBtn.addEventListener(\"click\", () => {\r\n\t\tformModal.style.display = \"block\";\r\n\t});\r\n\r\n\taddTaskBtn.addEventListener(\"click\", () => {\r\n\t\tformModal.style.display = \"none\";\r\n\t\tdashboard.removeChild(formModal);\r\n\t});\r\n\r\n\tcloseBtn.addEventListener(\"click\", () => {\r\n\t\tformModal.style.display = \"none\";\r\n\t\tdashboard.removeChild(formModal);\r\n\t});\r\n\r\n\twindow.addEventListener(\"click\", () => {\r\n\t\tif (event.target == formModal) {\r\n\t\t\tformModal.style.display = \"none\";\r\n\t\t\tdashboard.removeChild(formModal);\r\n\t\t}\r\n\t});\r\n\r\n\ttaskTitlePara.appendChild(taskTitleField);\r\n\ttaskDescPara.appendChild(taskDescField);\r\n\tfieldDiv.appendChild(taskTitlePara);\r\n\tfieldDiv.appendChild(taskDescPara);\r\n\ttaskHeaderSection.appendChild(closeBtn);\r\n\ttaskHeaderSection.appendChild(taskTitle);\r\n\tformContent.appendChild(taskHeaderSection);\r\n\tformContent.appendChild(fieldDiv);\r\n\tformContent.appendChild(addTaskBtn);\r\n\tformModal.appendChild(formContent);\r\n\tdashboard.appendChild(addProjectBtn);\r\n\tdashboard.appendChild(formModal);\r\n\r\n\treturn dashboard;\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (createProject);\r\n\n\n//# sourceURL=webpack://todo-list/./src/createProjectBtn.js?");
+
+/***/ }),
+
+/***/ "./src/createTask.js":
+/*!***************************!*\
+  !*** ./src/createTask.js ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _formDetails_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./formDetails.js */ \"./src/formDetails.js\");\n\r\n\r\nfunction createTask(task) {\r\n\tconst taskContainer = document.getElementById(\"task-container\");\r\n\r\n\tconst taskDiv = document.createElement(\"div\");\r\n\ttaskDiv.classList.add(\"task-div\");\r\n\ttaskDiv.setAttribute(\"id\", _formDetails_js__WEBPACK_IMPORTED_MODULE_0__.taskList.indexOf(task));\r\n\r\n\tconst taskCheckBox = document.createElement(\"input\");\r\n\ttaskCheckBox.classList.add(\"task-checkbox\");\r\n\ttaskCheckBox.setAttribute(\"type\", \"checkbox\");\r\n\ttaskCheckBox.setAttribute(\"name\", `task${_formDetails_js__WEBPACK_IMPORTED_MODULE_0__.taskList.indexOf(task)}`);\r\n\r\n\tconst taskTitle = document.createElement(\"h3\");\r\n\ttaskTitle.classList.add(\"task-title\");\r\n\ttaskTitle.textContent = task.title;\r\n\r\n\tconst taskDesc = document.createElement(\"p\");\r\n\ttaskDesc.textContent = task.description;\r\n\r\n\ttaskDiv.appendChild(taskCheckBox);\r\n\ttaskDiv.appendChild(taskTitle);\r\n\ttaskDiv.appendChild(taskDesc);\r\n\ttaskContainer.appendChild(taskDiv);\r\n\r\n\treturn taskContainer;\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (createTask);\r\n\n\n//# sourceURL=webpack://todo-list/./src/createTask.js?");
+
+/***/ }),
+
+/***/ "./src/displayTask.js":
+/*!****************************!*\
+  !*** ./src/displayTask.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _formDetails__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./formDetails */ \"./src/formDetails.js\");\n/* harmony import */ var _createTask__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./createTask */ \"./src/createTask.js\");\n\r\n\r\n\r\nfunction render() {\r\n\tconst taskContainer = document.getElementById(\"task-container\");\r\n\tconst tasks = document.querySelectorAll(\".task-div\");\r\n\r\n\ttasks.forEach((task) => taskContainer.removeChild(task));\r\n\r\n\tfor (let i = 0; i < _formDetails__WEBPACK_IMPORTED_MODULE_0__.taskList.length; i++) {\r\n\t\t(0,_createTask__WEBPACK_IMPORTED_MODULE_1__.default)(_formDetails__WEBPACK_IMPORTED_MODULE_0__.taskList[i]);\r\n\t}\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (render);\r\n\n\n//# sourceURL=webpack://todo-list/./src/displayTask.js?");
+
+/***/ }),
+
+/***/ "./src/formDetails.js":
+/*!****************************!*\
+  !*** ./src/formDetails.js ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"taskList\": () => (/* binding */ taskList),\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _displayTask__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./displayTask */ \"./src/displayTask.js\");\n\r\n\r\nlet taskList = [];\r\n\r\nfunction addProjectDetails() {\r\n\tlet newTask;\r\n\tclass Task {\r\n\t\tconstructor(title, description) {\r\n\t\t\t(this.title = form.title.value),\r\n\t\t\t\t(this.description = form.description.value);\r\n\t\t}\r\n\t}\r\n\r\n\tfunction addTask(title, description) {\r\n\t\tevent.preventDefault();\r\n\r\n\t\tnewTask = new Task(title, description);\r\n\t\ttaskList.push(newTask);\r\n\t\t(0,_displayTask__WEBPACK_IMPORTED_MODULE_0__.default)();\r\n\t\tform.reset();\r\n\t}\r\n\r\n\treturn addTask();\r\n}\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (addProjectDetails);\r\n\n\n//# sourceURL=webpack://todo-list/./src/formDetails.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _UIcomp_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./UIcomp.js */ \"./src/UIcomp.js\");\n\r\n\r\n(0,_UIcomp_js__WEBPACK_IMPORTED_MODULE_0__.default)();\r\n\n\n//# sourceURL=webpack://todo-list/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	
+/******/ })()
+;
