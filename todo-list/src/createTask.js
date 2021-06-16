9ecfb9e1e65fd502ef47076e9dc069ef868ba033
prLorence <paulorencereyes@gmail.com>
2021-06-16 11:07:15 +0800

    added checkbox functionality

--- todo-list/src/createTask.js
+++ todo-list/src/createTask.js
@@ -1,15 +1,30 @@
-import {taskList} from "./formDetails.js"
+import { taskList } from "./formDetails.js";
 
 function createTask(task) {
-  const taskContainer = document.getElementById("task-container")
+	const taskContainer = document.getElementById("task-container");
 
-  const taskDiv = document.createElement("div")
-  taskDiv.classList.add("task-div")
+	const taskDiv = document.createElement("div");
+	taskDiv.classList.add("task-div");
 	taskDiv.setAttribute("id", taskList.indexOf(task));
-  
-  const doneBtn = document.createElement("input")
-  doneBtn.classList.add("checkbox")
-  doneBtn.setAttribute("type", "checkbox")
+
+	const taskCheckBox = document.createElement("input");
+	taskCheckBox.classList.add("task-checkbox");
+	taskCheckBox.setAttribute("type", "checkbox");
+	taskCheckBox.setAttribute("name", `task${taskList.indexOf(task)}`);
+
+	const taskTitle = document.createElement("h3");
+	taskTitle.classList.add("task-title");
+	taskTitle.textContent = task.title;
+
+	const taskDesc = document.createElement("p");
+	taskDesc.textContent = task.description;
+
+	taskDiv.appendChild(taskCheckBox);
+	taskDiv.appendChild(taskTitle);
+	taskDiv.appendChild(taskDesc);
+	taskContainer.appendChild(taskDiv);
+
+	return taskContainer;
 }
 
-
+export default createTask;
