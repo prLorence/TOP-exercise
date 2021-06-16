f1f93c7a87be853c652beb5152b50fdd08ab5bf1
prLorence <paulorencereyes@gmail.com>
2021-06-16 11:09:17 +0800

    exported taskList as a global variable

--- todo-list/src/formDetails.js
+++ todo-list/src/formDetails.js
@@ -7,18 +7,19 @@ function addProjectDetails() {
 	class Task {
 		constructor(title, description) {
 			(this.title = form.title.value),
-			(this.description = form.description.value);
+				(this.description = form.description.value);
 		}
 	}
-	
+
 	function addTask(title, description) {
 		event.preventDefault();
-		
+
 		newTask = new Task(title, description);
 		taskList.push(newTask);
 		render();
+		form.reset();
 	}
-	
+
 	return addTask();
 }
 
